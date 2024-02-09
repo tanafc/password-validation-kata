f24cd150da26bb8bdb49f79200e35e6c4beb1dce
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-02-09 17:49:47 +0000

    red: validates that the password is long enough

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,12 +1,28 @@
 import { describe, it, expect } from "vitest"
 import { validatePassword } from "./main.js"
 
+// Tener más de 8 caracteres
+// Contener una letra mayúscula
+// Contener una letra minúscula
+// Contener un número
+// Contener un guión bajo
+
+// True => Password_1
+
 describe("Password validator", () => {
   it("validates a password", () => {
-    const password = "dummy"
+    const password = "Password_1"
 
     const result = validatePassword(password)
 
     expect(result).toBe(true)
+  })
+
+  it("validates that the password is long enough", () => {
+    const incorrectPassword = "Passw_1"
+
+    const result = validatePassword(incorrectPassword)
+
+    expect(result).toBe(false)
   })
 })
