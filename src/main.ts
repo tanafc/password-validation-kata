bc34e33a73d83b925b8b0906f1b109aaa285a442
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-02-09 18:19:28 +0000

    green: validates that the password contains a lowercase character

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 export class Password {
   private static readonly minLength: number = 8
 
+  static ensureIsValid(password: string) {
+    return (
+      this.ensureIsLongEnough(password) && this.ensureHasAnUppercase(password) && password !== password.toUpperCase()
+    )
+  }
   private static ensureIsLongEnough(password: string) {
     return password.length > this.minLength
   }
 
   private static ensureHasAnUppercase(password: string) {
     return password.toLowerCase() !== password
-  }
-
-  static ensureIsValid(password: string) {
-    return this.ensureIsLongEnough(password) && this.ensureHasAnUppercase(password)
   }
 }
